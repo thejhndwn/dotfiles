74da1610b7bdeef746b46b5028df7dbdd1ebbf5d
John <duanjohn99@gmail.com>
2025-03-19 21:35:58 -0400

    adding some installation stuff and some command reference stuff

---
+++ install/init.sh
@@ -0,0 +1,7 @@
+
+
+# prep the directory to be scp over to the server
+#
+# grab the PAT token 
+#
+# send over the directory with install.sh, PAT token, and a decrypt.sh
---
+++ scripts/decrypt.sh
@@ -0,0 +1,1 @@
+# some stuff here for decrypting and maybe encrypting with gpg
