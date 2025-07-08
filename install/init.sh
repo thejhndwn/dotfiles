5e86992065f5556f04012eccba0fbac0b37df5d4
John <duanjohn99@gmail.com>
2025-07-08 11:40:32 -0400

    starting fresh

--- install/init.sh
+++
@@ -1,7 +0,0 @@
-
-
-# prep the directory to be scp over to the server
-#
-# grab the PAT token 
-#
-# send over the directory with install.sh, PAT token, and a decrypt.sh
--- scripts/decrypt.sh
+++
@@ -1,1 +0,0 @@
-# some stuff here for decrypting and maybe encrypting with gpg
